f734aa8f234e68dc328d9255898ca34fc5b08ae1
nd0ut <nd0ut.me@gmail.com>
2021-12-03 14:58:01 +0300

    chore: update bundle

---
+++ copy-bundle.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cp -r ./upload-blocks/* ./angular-upload-blocks/src/upload-blocks/
+cp -r ./upload-blocks/* ./react-upload-blocks/src/upload-blocks/
+cp -r ./upload-blocks/* ./svelte-upload-blocks/upload-blocks/
+cp -r ./upload-blocks/* ./vue-upload-blocks/upload-blocks/
--- vue-upload-blocks/src/components/Uploader.vue
+++ vue-upload-blocks/src/components/Uploader.vue
@@ -1,24 +1,7 @@
 <template>
   <div class="wrapper">
     <div class="uploader">
-      <uc-simple-btn class="uc-wgt-common"></uc-simple-btn>
-
-      <uc-modal class="uc-wgt-common" strokes>
-        <uc-source-select>
-          <uc-source-list wrap></uc-source-list>
-          <uc-drop-area></uc-drop-area>
-        </uc-source-select>
-        <uc-upload-list></uc-upload-list>
-        <uc-camera-source></uc-camera-source>
-        <uc-url-source></uc-url-source>
-        <uc-external-source></uc-external-source>
-        <uc-upload-details></uc-upload-details>
-        <uc-confirmation-dialog></uc-confirmation-dialog>
-        <uc-cloud-image-editor></uc-cloud-image-editor>
-      </uc-modal>
-
-      <uc-message-box class="uc-wgt-common"></uc-message-box>
-      <uc-progress-bar class="uc-wgt-common"></uc-progress-bar>
+      <uc-default-widget class="uc-wgt-common"/>
 
       <uc-data-output
         @data-output="handleUploaderEvent"
