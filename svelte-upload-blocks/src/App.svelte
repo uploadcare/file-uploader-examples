711e6cacf06ead850f584078e9a20c3a7d373635
nd0ut <nd0ut.me@gmail.com>
2021-12-03 18:51:05 +0300

    fix: svelte template

--- svelte-upload-blocks/src/App.svelte
+++ svelte-upload-blocks/src/App.svelte
@@ -1,48 +1,31 @@
 <script>
 import '../upload-blocks/uc-basic.css';
 import '../upload-blocks/upload-blocks.js';
-	let files = []
-	function handleUploaderEvent(e) {
-		      const { data } = e.detail;
-      files = data;
 
-	}
+let files = []
+function handleUploaderEvent(e) {
+  const { data } = e.detail;
+    files = data;
+}
 </script>
 
 <div class="wrapper">
     <div class="uploader">
-      <uc-simple-btn class="uc-wgt-common"></uc-simple-btn>
-
-      <uc-modal class="uc-wgt-common" strokes>
-        <uc-source-select>
-          <uc-source-list wrap></uc-source-list>
-          <uc-drop-area></uc-drop-area>
-        </uc-source-select>
-        <uc-upload-list></uc-upload-list>
-        <uc-camera-source></uc-camera-source>
-        <uc-url-source></uc-url-source>
-        <uc-external-source></uc-external-source>
-        <uc-upload-details></uc-upload-details>
-        <uc-confirmation-dialog></uc-confirmation-dialog>
-        <uc-cloud-image-editor></uc-cloud-image-editor>
-      </uc-modal>
-
-      <uc-message-box class="uc-wgt-common"></uc-message-box>
-      <uc-progress-bar class="uc-wgt-common"></uc-progress-bar>
-
+      <uc-default-widget class="uc-wgt-common"/>
       <uc-data-output
         fire-event
         class="uc-wgt-common"
-				on:data-output={handleUploaderEvent}
+        on:data-output={handleUploaderEvent}
       ></uc-data-output>
     </div>
     <div class="output">
-			{#each files as file}
+      {#each files as file}
       <img
         src="https://ucarecdn.com/{file.uuid}/-/preview/-/scale_crop/400x400/"
         width="200"
+        alt="Uploadcare uploaded file"
       />
-			{/each}
+      {/each}
     </div>
 </div>
 
