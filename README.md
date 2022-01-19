82c95eae31311825a8a23314dda845852e22b1da
nd0ut <nd0ut.me@gmail.com>
2022-01-19 14:56:11 +0300

    fix: react example

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Upload Blocks examples
 
 * [Svelte](https://codesandbox.io/s/github/uploadcare/upload-blocks-examples/tree/main/svelte-upload-blocks)
-* [Angular](https://codesandbox.io/s/github/uploadcare/upload-blocks-examples/tree/main/angular-upload-blocks) (something bad with styles)
+* [Angular](https://codesandbox.io/s/github/uploadcare/upload-blocks-examples/tree/main/angular-upload-blocks)
 * [React](https://codesandbox.io/s/github/uploadcare/upload-blocks-examples/tree/main/react-upload-blocks)
 * [Vue](https://codesandbox.io/s/github/uploadcare/upload-blocks-examples/tree/main/vue-upload-blocks)
--- react-upload-blocks/src/App.js
+++ react-upload-blocks/src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import { useState } from "react";
-import "./upload-blocks/uc-basic.css";
-import "./upload-blocks/upload-blocks.js";
+import "@uploadcare/upload-blocks/build/uc-basic.css";
+import "@uploadcare/upload-blocks";
 
 function App() {
   const [files, setFiles] = useState([]);
@@ -12,7 +12,26 @@ function App() {
   return (
     <div className="wrapper">
       <div className="uploader">
-        <uc-default-widget class="uc-wgt-common"/>
+        <uc-simple-btn class="uc-wgt-common"></uc-simple-btn>
+
+        <uc-modal strokes class="uc-wgt-common">
+          <uc-activity-icon slot="heading"></uc-activity-icon>
+          <uc-activity-caption slot="heading"></uc-activity-caption>
+          <uc-start-from>
+            <uc-source-list wrap></uc-source-list>
+            <uc-drop-area></uc-drop-area>
+          </uc-start-from>
+          <uc-upload-list></uc-upload-list>
+          <uc-camera-source></uc-camera-source>
+          <uc-url-source></uc-url-source>
+          <uc-external-source></uc-external-source>
+          <uc-upload-details></uc-upload-details>
+          <uc-confirmation-dialog></uc-confirmation-dialog>
+        </uc-modal>
+
+        <uc-message-box class="uc-wgt-common"></uc-message-box>
+        <uc-progress-bar class="uc-wgt-common"></uc-progress-bar>
+
         <uc-data-output
           fire-event
           class="uc-wgt-common"
