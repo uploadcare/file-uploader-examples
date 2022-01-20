65d15b2f81d946569af79c6329b0ee97af7d7b6b
nd0ut <nd0ut.me@gmail.com>
2022-01-20 17:03:30 +0300

    Revert "fix(react): use uc-uploader"
    This reverts commit 5380a4c2bd99c8c5b883719cf6185fed4a36e505.

--- react-upload-blocks/src/App.js
+++ react-upload-blocks/src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import { useState } from "react";
-import "@uploadcare/uploader/build/regular/uc-uploader.css";
-import "@uploadcare/uploader/build/regular/uc-uploader.min.js";
+import "@uploadcare/upload-blocks/build/uc-basic.css";
+import "@uploadcare/upload-blocks";
 
 function App() {
   const [files, setFiles] = useState([]);
@@ -12,7 +12,25 @@ function App() {
   return (
     <div className="wrapper">
       <div className="uploader">
-        <uc-uploader class="uc-wgt-common"></uc-uploader>
+        <uc-simple-btn class="uc-wgt-common"></uc-simple-btn>
+
+        <uc-modal strokes class="uc-wgt-common">
+          <uc-activity-icon slot="heading"></uc-activity-icon>
+          <uc-activity-caption slot="heading"></uc-activity-caption>
+          <uc-start-from>
+            <uc-source-list wrap></uc-source-list>
+            <uc-drop-area></uc-drop-area>
+          </uc-start-from>
+          <uc-upload-list></uc-upload-list>
+          <uc-camera-source></uc-camera-source>
+          <uc-url-source></uc-url-source>
+          <uc-external-source></uc-external-source>
+          <uc-upload-details></uc-upload-details>
+          <uc-confirmation-dialog></uc-confirmation-dialog>
+        </uc-modal>
+
+        <uc-message-box class="uc-wgt-common"></uc-message-box>
+        <uc-progress-bar class="uc-wgt-common"></uc-progress-bar>
 
         <uc-data-output
           fire-event
