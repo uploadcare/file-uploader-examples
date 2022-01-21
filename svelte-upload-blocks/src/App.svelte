00ff59e6a029deeb5d2644dddb9e7e292b5f0877
nd0ut <nd0ut.me@gmail.com>
2022-01-21 11:47:15 +0300

    fix(svelte): use uc-uploader

--- svelte-upload-blocks/src/App.svelte
+++ svelte-upload-blocks/src/App.svelte
@@ -1,6 +1,6 @@
 <script>
-  import "@uploadcare/upload-blocks/build/uc-basic.css";
-  import "@uploadcare/upload-blocks";
+  import "@uploadcare/uploader/build/regular/uc-uploader.css";
+  import "@uploadcare/uploader/build/regular/uc-uploader.min.js";
 
   let files = [];
   function handleUploaderEvent(e) {
@@ -10,32 +10,13 @@
 </script>
 
 <div class="wrapper">
-  <div class="uploader">
-    <uc-simple-btn class="uc-wgt-common" />
-    <uc-modal strokes class="uc-wgt-common">
-      <uc-activity-icon slot="heading" />
-      <uc-activity-caption slot="heading" />
-      <uc-start-from>
-        <uc-source-list wrap />
-        <uc-drop-area />
-      </uc-start-from>
-      <uc-upload-list />
-      <uc-camera-source />
-      <uc-url-source />
-      <uc-external-source />
-      <uc-upload-details />
-      <uc-confirmation-dialog />
-    </uc-modal>
+  <uc-uploader class="uploader-cfg uc-wgt-common" />
 
-    <uc-message-box class="uc-wgt-common" />
-    <uc-progress-bar class="uc-wgt-common" />
-
-    <uc-data-output
-      fire-event
-      class="uc-wgt-common"
-      on:data-output={handleUploaderEvent}
-    />
-  </div>
+  <uc-data-output
+    fire-event
+    class="uploader-cfg uc-wgt-common"
+    on:data-output={handleUploaderEvent}
+  />
   <div class="output">
     {#each files as file}
       <img
@@ -60,8 +41,7 @@
     width: 100%;
     max-width: 1000px;
   }
-  .uploader {
-    --ctx-name: 'my-uploader';
+  .uploader-cfg {
     --cfg-pubkey: "demopublickey";
     --cfg-multiple: 1;
     --cfg-confirm-upload: 1;
@@ -69,7 +49,7 @@
     --cfg-accept: "";
     --cfg-store: 1;
     --cfg-camera-mirror: 0;
-    --cfg-source-list: "local, url, camera, draw, dropbox, gdrive, facebook";
+    --cfg-source-list: "local, url, camera, draw, dropbox, gdrive";
     --cfg-max-files: 10;
   }
 </style>
