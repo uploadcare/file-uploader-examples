6e8ac668524c7d2b66b97beaf3220ee477b2fb1f
nd0ut <nd0ut.me@gmail.com>
2022-01-31 13:31:34 +0300

    chore: remove upload-blocks local bundle

--- copy-bundle.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cp -r ./upload-blocks/* ./angular-upload-blocks/src/upload-blocks/
-cp -r ./upload-blocks/* ./react-upload-blocks/src/upload-blocks/
-cp -r ./upload-blocks/* ./svelte-upload-blocks/upload-blocks/
-cp -r ./upload-blocks/* ./vue-upload-blocks/upload-blocks/
