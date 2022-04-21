e27c386b51581b1206e84bed529d1f1fe43f53f1
nd0ut <nd0ut.me@gmail.com>
2022-04-21 13:52:13 +0300

    chore: update react example

--- react-uploader/src/App.js
+++ react-uploader/src/App.js
@@ -1,27 +1,40 @@
-import { useState } from "react";
-import "@uploadcare/uploader/build/regular/uc-uploader.css";
-import "@uploadcare/uploader/build/regular/uc-uploader.min.js";
-import st from "./App.module.css";
+import { useState, useCallback, useRef, useEffect } from "react";
+import * as UC from "@uploadcare/uc-blocks";
+
 // Order of css imports is important. User styles should be loaded after the main theme
 // Or config selector should be more specific.
+import "@uploadcare/uc-blocks/solutions/file-uploader/regular/index.css";
+import st from "./App.module.css";
+
+UC.registerBlocks(UC);
 
 function App() {
+  let dataOutputRef = useRef();
   const [files, setFiles] = useState([]);
-  const handleUploaderEvent = (e) => {
+  const handleUploaderEvent = useCallback((e) => {
     const { data } = e.detail;
     setFiles(data);
-  };
+  }, []);
+
+  useEffect(() => {
+    let el = dataOutputRef.current;
+    el.addEventListener("data-output", handleUploaderEvent);
+    return () => {
+      el.removeEventListener("data-output", handleUploaderEvent);
+    };
+  }, [handleUploaderEvent]);
 
   const classNames = ["uc-wgt-common", st.uploaderCfg].join(" ");
 
   return (
     <div className={st.wrapper}>
-      <uc-uploader class={classNames}></uc-uploader>
+      <uc-file-uploader-regular class={classNames}></uc-file-uploader-regular>
 
       <uc-data-output
+        ref={dataOutputRef}
         fire-event
         class={classNames}
-        onDataOutput={handleUploaderEvent}
+        onEvent={handleUploaderEvent}
       ></uc-data-output>
 
       <div className={st.output}>
@@ -30,6 +43,7 @@ function App() {
             key={file.uuid}
             src={`https://ucarecdn.com/${file.uuid}/-/preview/-/scale_crop/400x400/`}
             width="200"
+            alt="Preview"
           />
         ))}
       </div>
