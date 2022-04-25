04fc04f9f91ed79d56d290408c5d1533a75265cd
nd0ut <nd0ut.me@gmail.com>
2022-04-25 18:38:24 +0300

    chore: use uc-blocks types inside angular example

--- examples/angular-uploader/src/app/app.component.ts
+++ examples/angular-uploader/src/app/app.component.ts
@@ -1,8 +1,6 @@
 import { Component, ViewEncapsulation } from '@angular/core';
-
-// TODO: remove @ts-expect-error when we'll release the types
-// @ts-expect-error
 import * as UC from '@uploadcare/uc-blocks';
+import type { UploadcareFile } from '@uploadcare/uc-blocks/submodules/upload-client/upload-client.js';
 
 UC.registerBlocks(UC);
 
@@ -10,11 +8,10 @@ UC.registerBlocks(UC);
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  encapsulation: ViewEncapsulation.ShadowDom
+  encapsulation: ViewEncapsulation.ShadowDom,
 })
 export class AppComponent {
-  // TODO: fix any, use proper type when it will be exported
-  files: any[] = [];
+  files: UploadcareFile[] = [];
 
   handleUploaderEvent(e: Event) {
     const { data: files } = (e as CustomEvent).detail;
