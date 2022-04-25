6375f29976ea7aab36bfd0be7a6c0a2fb16e0d76
nd0ut <nd0ut.me@gmail.com>
2022-04-25 12:44:13 +0300

    chore: add comments about typings

--- angular-uploader/src/app/app.component.ts
+++ angular-uploader/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, ViewEncapsulation } from '@angular/core';
+
+// TODO: remove @ts-expect-error when we'll release the types
 // @ts-expect-error
 import * as UC from '@uploadcare/uc-blocks';
 
@@ -11,6 +13,7 @@ UC.registerBlocks(UC);
   encapsulation: ViewEncapsulation.ShadowDom
 })
 export class AppComponent {
+  // TODO: fix any, use proper type when it will be exported
   files: any[] = [];
 
   handleUploaderEvent(e: Event) {
