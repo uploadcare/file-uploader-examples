1a538937ba05934ef62a914771a325c9132fc958
igoradamenko <mail@igoradamenko.com>
2023-10-23 17:31:18 +0300

    Add form mocks

---
+++ examples/react-uploader/src/mocks.ts
@@ -0,0 +1,310 @@
+export default {
+  title: 'A Romantic Weekend Getaway in Paris',
+  text: `Paris, often referred to as the "City of Love" and the "City of Lights," is a dream destination for many travelers around the world. With its rich history, stunning architecture, delectable cuisine, and an unmistakable romantic atmosphere, Paris offers an unforgettable experience. In this blog post, we'll take you on a journey through our recent weekend trip to Paris, where we gazed at the iconic Eiffel Tower, indulged in heavenly croissants, and immersed ourselves in the city's captivating charm.
+
+# Day 1: Arrival in the City of Love
+
+...`,
+  photos: [
+    {
+      uuid: 'e456f8cd-a717-4894-932d-e1d39e043e50',
+      name: 'arthurhumeau3xwdarHxHqIunsplash.jpg',
+      size: 1657550,
+      isStored: true,
+      isImage: true,
+      mimeType: 'image/jpeg',
+      cdnUrl: 'https://ucarecdn.com/e456f8cd-a717-4894-932d-e1d39e043e50/',
+      s3Url: null,
+      originalFilename: 'arthur-humeau-3xwdarHxHqI-unsplash.jpg',
+      imageInfo: {
+        dpi: [
+          72,
+          72
+        ],
+        width: 2400,
+        format: 'JPEG',
+        height: 3600,
+        sequence: false,
+        colorMode: 'RGB',
+        orientation: null,
+        geoLocation: null,
+        datetimeOriginal: null
+      },
+      videoInfo: null,
+      contentInfo: {
+        mime: {
+          mime: 'image/jpeg',
+          type: 'image',
+          subtype: 'jpeg'
+        },
+        image: {
+          dpi: [
+            72,
+            72
+          ],
+          width: 2400,
+          format: 'JPEG',
+          height: 3600,
+          sequence: false,
+          colorMode: 'RGB',
+          orientation: null,
+          geoLocation: null,
+          datetimeOriginal: null
+        }
+      },
+      metadata: {},
+      s3Bucket: null,
+      cdnUrlModifiers: null
+    },
+    {
+      uuid: '097d3484-3d33-4f2f-805f-7eea0d915e98',
+      name: 'bharatpatilWR5_Ev_bhIunsplash.jpg',
+      size: 974188,
+      isStored: true,
+      isImage: true,
+      mimeType: 'image/jpeg',
+      cdnUrl: 'https://ucarecdn.com/097d3484-3d33-4f2f-805f-7eea0d915e98/',
+      s3Url: null,
+      originalFilename: 'bharat-patil-WR5_Ev_bh-I-unsplash.jpg',
+      imageInfo: {
+        dpi: [
+          72,
+          72
+        ],
+        width: 2400,
+        format: 'JPEG',
+        height: 1600,
+        sequence: false,
+        colorMode: 'RGB',
+        orientation: null,
+        geoLocation: null,
+        datetimeOriginal: null
+      },
+      videoInfo: null,
+      contentInfo: {
+        mime: {
+          mime: 'image/jpeg',
+          type: 'image',
+          subtype: 'jpeg'
+        },
+        image: {
+          dpi: [
+            72,
+            72
+          ],
+          width: 2400,
+          format: 'JPEG',
+          height: 1600,
+          sequence: false,
+          colorMode: 'RGB',
+          orientation: null,
+          geoLocation: null,
+          datetimeOriginal: null
+        }
+      },
+      metadata: {},
+      s3Bucket: null,
+      cdnUrlModifiers: null
+    },
+    {
+      uuid: '5a70fbad-0661-4da1-b1e8-99e1633a5cd9',
+      name: 'chriskaridisnnzkZNYWHaUunsplash.jpg',
+      size: 535450,
+      isStored: true,
+      isImage: true,
+      mimeType: 'image/jpeg',
+      cdnUrl: 'https://ucarecdn.com/5a70fbad-0661-4da1-b1e8-99e1633a5cd9/',
+      s3Url: null,
+      originalFilename: 'chris-karidis-nnzkZNYWHaU-unsplash.jpg',
+      imageInfo: {
+        dpi: [
+          72,
+          72
+        ],
+        width: 2400,
+        format: 'JPEG',
+        height: 1596,
+        sequence: false,
+        colorMode: 'RGB',
+        orientation: null,
+        geoLocation: null,
+        datetimeOriginal: null
+      },
+      videoInfo: null,
+      contentInfo: {
+        mime: {
+          mime: 'image/jpeg',
+          type: 'image',
+          subtype: 'jpeg'
+        },
+        image: {
+          dpi: [
+            72,
+            72
+          ],
+          width: 2400,
+          format: 'JPEG',
+          height: 1596,
+          sequence: false,
+          colorMode: 'RGB',
+          orientation: null,
+          geoLocation: null,
+          datetimeOriginal: null
+        }
+      },
+      metadata: {},
+      s3Bucket: null,
+      cdnUrlModifiers: null
+    },
+    {
+      uuid: '54645000-d234-4ccf-ba49-df4a7237b071',
+      name: 'earthDXuxHw3S5akunsplash.jpg',
+      size: 673331,
+      isStored: true,
+      isImage: true,
+      mimeType: 'image/jpeg',
+      cdnUrl: 'https://ucarecdn.com/54645000-d234-4ccf-ba49-df4a7237b071/',
+      s3Url: null,
+      originalFilename: 'earth-DXuxHw3S5ak-unsplash.jpg',
+      imageInfo: {
+        dpi: [
+          72,
+          72
+        ],
+        width: 1600,
+        format: 'JPEG',
+        height: 2000,
+        sequence: false,
+        colorMode: 'RGB',
+        orientation: null,
+        geoLocation: null,
+        datetimeOriginal: null
+      },
+      videoInfo: null,
+      contentInfo: {
+        mime: {
+          mime: 'image/jpeg',
+          type: 'image',
+          subtype: 'jpeg'
+        },
+        image: {
+          dpi: [
+            72,
+            72
+          ],
+          width: 1600,
+          format: 'JPEG',
+          height: 2000,
+          sequence: false,
+          colorMode: 'RGB',
+          orientation: null,
+          geoLocation: null,
+          datetimeOriginal: null
+        }
+      },
+      metadata: {},
+      s3Bucket: null,
+      cdnUrlModifiers: null
+    },
+    {
+      uuid: 'c24b5d74-78ef-4cc3-a4b8-0ef73996cb74',
+      name: 'johntownerHf4Ap1ef40unsplash.jpg',
+      size: 1868140,
+      isStored: true,
+      isImage: true,
+      mimeType: 'image/jpeg',
+      cdnUrl: 'https://ucarecdn.com/c24b5d74-78ef-4cc3-a4b8-0ef73996cb74/',
+      s3Url: null,
+      originalFilename: 'john-towner-Hf4Ap1-ef40-unsplash.jpg',
+      imageInfo: {
+        dpi: [
+          72,
+          72
+        ],
+        width: 2400,
+        format: 'JPEG',
+        height: 3438,
+        sequence: false,
+        colorMode: 'RGB',
+        orientation: null,
+        geoLocation: null,
+        datetimeOriginal: null
+      },
+      videoInfo: null,
+      contentInfo: {
+        mime: {
+          mime: 'image/jpeg',
+          type: 'image',
+          subtype: 'jpeg'
+        },
+        image: {
+          dpi: [
+            72,
+            72
+          ],
+          width: 2400,
+          format: 'JPEG',
+          height: 3438,
+          sequence: false,
+          colorMode: 'RGB',
+          orientation: null,
+          geoLocation: null,
+          datetimeOriginal: null
+        }
+      },
+      metadata: {},
+      s3Bucket: null,
+      cdnUrlModifiers: null
+    },
+    {
+      uuid: '5e26e5dd-c115-4b1f-b14c-0ddc375b3ea5',
+      name: 'sebastiengabrielgyUVNafCIG8unsplash.jpg',
+      size: 1021096,
+      isStored: true,
+      isImage: true,
+      mimeType: 'image/jpeg',
+      cdnUrl: 'https://ucarecdn.com/5e26e5dd-c115-4b1f-b14c-0ddc375b3ea5/',
+      s3Url: null,
+      originalFilename: 'sebastien-gabriel-gyUVNafCIG8-unsplash.jpg',
+      imageInfo: {
+        dpi: [
+          72,
+          72
+        ],
+        width: 2400,
+        format: 'JPEG',
+        height: 3600,
+        sequence: false,
+        colorMode: 'RGB',
+        orientation: null,
+        geoLocation: null,
+        datetimeOriginal: null
+      },
+      videoInfo: null,
+      contentInfo: {
+        mime: {
+          mime: 'image/jpeg',
+          type: 'image',
+          subtype: 'jpeg'
+        },
+        image: {
+          dpi: [
+            72,
+            72
+          ],
+          width: 2400,
+          format: 'JPEG',
+          height: 3600,
+          sequence: false,
+          colorMode: 'RGB',
+          orientation: null,
+          geoLocation: null,
+          datetimeOriginal: null
+        }
+      },
+      metadata: {},
+      s3Bucket: null,
+      cdnUrlModifiers: null
+    }
+  ]
+}
