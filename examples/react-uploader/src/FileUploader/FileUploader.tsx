e799ea386ae7f2b49f0675c1bdc953ec05d740fe
igoradamenko <mail@igoradamenko.com>
2023-10-23 17:31:29 +0300

    Create FileUploader component

---
+++ examples/react-uploader/src/FileUploader/FileUploader.tsx
@@ -0,0 +1,96 @@
+import React, { useCallback, useEffect, useState } from 'react';
+import * as LR from '@uploadcare/blocks';
+import { PACKAGE_VERSION } from '@uploadcare/blocks';
+
+import { File } from '../types';
+
+import st from './FileUploader.module.css';
+import cssOverrides from './FileUploader.overrides.css?inline';
+
+LR.FileUploaderRegular.shadowStyles = cssOverrides;
+
+LR.registerBlocks(LR);
+
+type FileUploaderProps = {
+  uploaderClassName: string;
+  files: File[];
+  onChange: (files: File[]) => void;
+  maxAllowedFiles: number;
+}
+
+export default function FileUploader({ files, uploaderClassName, onChange, maxAllowedFiles }: FileUploaderProps) {
+  const [resetCounter, setResetCounter] = useState(0);
+
+  const handleRemoveClick = useCallback(
+    (uuid: File['uuid']) => onChange(files.filter(f => f.uuid !== uuid)),
+    [files, onChange],
+  );
+
+  useEffect(() => {
+    const resetUploaderState = () => setResetCounter(v => v + 1);
+
+    const handleUploadEvent = (e: CustomEvent<{ data: File[] }>) => {
+      if (e.detail?.data) {
+        onChange([...files, ...e.detail.data]);
+      }
+    };
+
+    const handleDoneFlow = () => {
+      resetUploaderState();
+    };
+
+    window.addEventListener('LR_DATA_OUTPUT', handleUploadEvent);
+    window.addEventListener('LR_DONE_FLOW', handleDoneFlow);
+
+    return () => {
+      window.removeEventListener('LR_DATA_OUTPUT', handleUploadEvent);
+      window.removeEventListener('LR_DONE_FLOW', handleDoneFlow);
+    };
+  }, [files, onChange]);
+
+  maxAllowedFiles = maxAllowedFiles - files.length;
+
+  const isMultipleAllowed = maxAllowedFiles > 1;
+
+  return (
+    <div className={st.root}>
+      <lr-config
+        ctx-name={`uploader-ctx-${resetCounter}`}
+        pubkey="demopublickey"
+        multiple={isMultipleAllowed}
+        sourceList="local, url, camera, dropbox, gdrive"
+        multipleMax={maxAllowedFiles}
+        confirmUpload={false}
+        removeCopyright={true}
+        imgOnly={true}
+      ></lr-config>
+
+      <lr-file-uploader-regular
+        ctx-name={`uploader-ctx-${resetCounter}`}
+        css-src={`https://unpkg.com/@uploadcare/blocks@${PACKAGE_VERSION}/web/lr-file-uploader-regular.min.css`}
+        class={uploaderClassName}
+      ></lr-file-uploader-regular>
+
+      <div className={st.previews}>
+        {files.map((file) => (
+          <div key={file.uuid} className={st.preview}>
+            <img
+              className={st.previewImage}
+              key={file.uuid}
+              src={`https://ucarecdn.com/${file.uuid}/-/preview/-/resize/x200/`}
+              width="100"
+              alt={file.originalFilename}
+              title={file.originalFilename}
+            />
+
+            <button
+              className={st.previewRemoveButton}
+              type="button"
+              onClick={() => handleRemoveClick(file.uuid)}
+            >×</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
---
+++ examples/react-uploader/src/types.ts
@@ -0,0 +1,6 @@
+export type File = {
+  isImage: boolean;
+  cdnUrl: string;
+  originalFilename: string;
+  uuid: string;
+};
