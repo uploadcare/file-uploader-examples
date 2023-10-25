b6fb8af3f82ccc6957c442207b0dc990cb7073fd
igoradamenko <mail@igoradamenko.com>
2023-10-25 17:10:38 +0300

    Add important notes and README

--- examples/react-uploader/README.md
+++ examples/react-uploader/README.md
@@ -4,4 +4,44 @@
 
 ## Integration notes
 
-WIP
+Please, read [File Uploader documentation](https://uploadcare.com/docs/file-uploader/) first.
+
+It is easy to use Blocks in React app, because they are native to the Web. 
+However, you need to understand that they are not native to React.
+
+So, if you integrate Blocks with React, you need to decide which part of your solution will encapsulate non-React code.
+
+In this example we have created [FileUploader](./src/FileUploader/FileUploader.tsx) component 
+which provides React-friendly API for the rest of the app. There are Blocks inside of this component and nowhere else.
+
+### Installation
+
+All you need to do is to install [`@uploadcare/blocks`](https://www.npmjs.com/package/@uploadcare/blocks) from npm
+via your favorite Node package manager.
+
+The package provides TypeScript types, so you do not need to install `@types/anything` if you need a proper typing.
+
+[Read more about installation]((https://uploadcare.com/docs/file-uploader/installation/)) in the File Uploader documentation.
+
+### Non-React things you should know about
+
+The most common way to “communicate” with File Uploader written via Blocks is by [events](https://uploadcare.com/docs/file-uploader/data-and-events/).
+You will find them in the example. This may not be the way React apps live nowadays, but it's easy to handle using 
+hooks like [`useEffect`](https://react.dev/reference/react/useEffect).
+
+Another one thing to note is the attributes. 
+Please note, some attributes required by Blocks are kebab-cased, not camelCased as usual for React world.
+
+Plus, due to the fact that Blocks are native to Web, we use `class` attribute instead of `className`.
+
+Finally, you are able to invoke [[some methods of File Uploader](https://uploadcare.com/docs/file-uploader/api/) 
+to control its behavior. This is also non common to React world, but feel free to use them if you need.
+
+### Styling
+
+If your styling solution may provide class string or style object, feel free to use them on blocks like 
+`lr-file-uploader-regular` and override default class using CSS variables.
+
+Otherwise you may go “full override” way and pass a string with styles to a File Uploader type of your choice.
+
+[Read more about styling](https://uploadcare.com/docs/file-uploader/styling/) in the File Uploader docs.
--- examples/react-uploader/src/FileUploader/FileUploader.tsx
+++ examples/react-uploader/src/FileUploader/FileUploader.tsx
@@ -8,6 +8,14 @@ import st from './FileUploader.module.scss';
 import cssOverrides from './FileUploader.overrides.css?inline';
 import cs from 'classnames';
 
+/*
+  Note: File Uploader styles are scoped due to ShadowDOM usage.
+  There are two ways to override them. One way is used on the line below,
+  another one is to set a custom class to File Uploader,
+  and use CSS variables to update styles.
+
+  See more: https://uploadcare.com/docs/file-uploader/styling/
+ */
 LR.FileUploaderRegular.shadowStyles = cssOverrides;
 
 LR.registerBlocks(LR);
@@ -21,6 +29,16 @@ type FileUploaderProps = {
 }
 
 export default function FileUploader({ files, uploaderClassName, onChange, maxAllowedFiles, theme }: FileUploaderProps) {
+  /*
+    Note: Here we use a counter to reset File Uploader state.
+    It's not necessary though. We use it here to show users
+    a fresh version of File Uploader every time they open it.
+
+    Another way is to sync File Uploader state with an external store.
+    You can manipulate File Uploader using API calls like `addFileFromObject`, etc.
+
+    See more: https://uploadcare.com/docs/file-uploader/api/
+   */
   const [resetCounter, setResetCounter] = useState(0);
 
   const handleRemoveClick = useCallback(
@@ -41,6 +59,12 @@ export default function FileUploader({ files, uploaderClassName, onChange, maxAl
       resetUploaderState();
     };
 
+    /*
+      Note: Event binding is the main way to get data and other info from File Uploader.
+      There plenty of events you may use.
+
+      See more: https://uploadcare.com/docs/file-uploader/data-and-events/#events
+     */
     window.addEventListener('LR_DATA_OUTPUT', handleUploadEvent);
     window.addEventListener('LR_DONE_FLOW', handleDoneFlow);
 
@@ -56,6 +80,18 @@ export default function FileUploader({ files, uploaderClassName, onChange, maxAl
 
   return (
     <div className={st.root}>
+      {/*
+         Note: `lr-config` is the main block we use to configure File Uploader.
+         It's important to all the context-related blocks to have the same `ctx-name` attribute.
+
+         See more: https://uploadcare.com/docs/file-uploader/configuration/
+         Available options: https://uploadcare.com/docs/file-uploader/options/
+
+         Also note: Some options currently are not available via `lr-config`,
+         but may be set via CSS properties. E.g. `darkmode`.
+
+         Here they are: https://github.com/uploadcare/blocks/blob/main/blocks/themes/lr-basic/config.css
+      */}
       <lr-config
         ctx-name={`uploader-ctx-${resetCounter}`}
         pubkey="demopublickey"
