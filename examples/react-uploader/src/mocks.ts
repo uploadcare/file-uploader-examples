ba5d13350ccaefa93ee222dc68568e7286818487
igoradamenko <mail@igoradamenko.com>
2023-10-26 12:49:40 +0300

    Reupload images to Uploadcare account

--- examples/react-uploader/src/mocks.ts
+++ examples/react-uploader/src/mocks.ts
@@ -7,13 +7,13 @@ export default {
 ...`,
   photos: [
     {
-      uuid: 'e456f8cd-a717-4894-932d-e1d39e043e50',
+      uuid: 'add046ce-95e3-4454-bccd-65e3e648c472',
       name: 'arthurhumeau3xwdarHxHqIunsplash.jpg',
       size: 1657550,
       isStored: true,
       isImage: true,
       mimeType: 'image/jpeg',
-      cdnUrl: 'https://ucarecdn.com/e456f8cd-a717-4894-932d-e1d39e043e50/',
+      cdnUrl: 'https://ucarecdn.com/add046ce-95e3-4454-bccd-65e3e648c472/',
       s3Url: null,
       originalFilename: 'arthur-humeau-3xwdarHxHqI-unsplash.jpg',
       imageInfo: {
@@ -57,13 +57,13 @@ export default {
       cdnUrlModifiers: null
     },
     {
-      uuid: '097d3484-3d33-4f2f-805f-7eea0d915e98',
+      uuid: '9732c6a8-7199-418d-9e02-2a384a953363',
       name: 'bharatpatilWR5_Ev_bhIunsplash.jpg',
       size: 974188,
       isStored: true,
       isImage: true,
       mimeType: 'image/jpeg',
-      cdnUrl: 'https://ucarecdn.com/097d3484-3d33-4f2f-805f-7eea0d915e98/',
+      cdnUrl: 'https://ucarecdn.com/9732c6a8-7199-418d-9e02-2a384a953363/',
       s3Url: null,
       originalFilename: 'bharat-patil-WR5_Ev_bh-I-unsplash.jpg',
       imageInfo: {
@@ -107,13 +107,13 @@ export default {
       cdnUrlModifiers: null
     },
     {
-      uuid: '5a70fbad-0661-4da1-b1e8-99e1633a5cd9',
+      uuid: '7708b679-644d-40ba-8b53-5d91bac5125d',
       name: 'chriskaridisnnzkZNYWHaUunsplash.jpg',
       size: 535450,
       isStored: true,
       isImage: true,
       mimeType: 'image/jpeg',
-      cdnUrl: 'https://ucarecdn.com/5a70fbad-0661-4da1-b1e8-99e1633a5cd9/',
+      cdnUrl: 'https://ucarecdn.com/7708b679-644d-40ba-8b53-5d91bac5125d/',
       s3Url: null,
       originalFilename: 'chris-karidis-nnzkZNYWHaU-unsplash.jpg',
       imageInfo: {
@@ -157,13 +157,13 @@ export default {
       cdnUrlModifiers: null
     },
     {
-      uuid: '54645000-d234-4ccf-ba49-df4a7237b071',
+      uuid: '695dd86e-6095-4e04-9dab-89c082fdba57',
       name: 'earthDXuxHw3S5akunsplash.jpg',
       size: 673331,
       isStored: true,
       isImage: true,
       mimeType: 'image/jpeg',
-      cdnUrl: 'https://ucarecdn.com/54645000-d234-4ccf-ba49-df4a7237b071/',
+      cdnUrl: 'https://ucarecdn.com/695dd86e-6095-4e04-9dab-89c082fdba57/',
       s3Url: null,
       originalFilename: 'earth-DXuxHw3S5ak-unsplash.jpg',
       imageInfo: {
@@ -207,13 +207,13 @@ export default {
       cdnUrlModifiers: null
     },
     {
-      uuid: 'c24b5d74-78ef-4cc3-a4b8-0ef73996cb74',
+      uuid: '8cbf5c6f-2e3b-4ad4-8321-2c2a7a7200cd',
       name: 'johntownerHf4Ap1ef40unsplash.jpg',
       size: 1868140,
       isStored: true,
       isImage: true,
       mimeType: 'image/jpeg',
-      cdnUrl: 'https://ucarecdn.com/c24b5d74-78ef-4cc3-a4b8-0ef73996cb74/',
+      cdnUrl: 'https://ucarecdn.com/8cbf5c6f-2e3b-4ad4-8321-2c2a7a7200cd/',
       s3Url: null,
       originalFilename: 'john-towner-Hf4Ap1-ef40-unsplash.jpg',
       imageInfo: {
@@ -257,13 +257,13 @@ export default {
       cdnUrlModifiers: null
     },
     {
-      uuid: '5e26e5dd-c115-4b1f-b14c-0ddc375b3ea5',
+      uuid: '69ebc69c-f24a-481c-a71f-ecf8fe4f80be',
       name: 'sebastiengabrielgyUVNafCIG8unsplash.jpg',
       size: 1021096,
       isStored: true,
       isImage: true,
       mimeType: 'image/jpeg',
-      cdnUrl: 'https://ucarecdn.com/5e26e5dd-c115-4b1f-b14c-0ddc375b3ea5/',
+      cdnUrl: 'https://ucarecdn.com/69ebc69c-f24a-481c-a71f-ecf8fe4f80be/',
       s3Url: null,
       originalFilename: 'sebastien-gabriel-gyUVNafCIG8-unsplash.jpg',
       imageInfo: {
