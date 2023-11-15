00e306cf2706bc38b98b9976b7ec1059b49dbb01
igoradamenko <mail@igoradamenko.com>
2023-11-15 14:03:05 +0200

    Set isCustomElement Vue compiler option to recognize Blocks

--- examples/vue-uploader/vite.config.js
+++ examples/vue-uploader/vite.config.js
@@ -6,7 +6,14 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue({
+      template: {
+        compilerOptions: {
+          // treat all tags with `lr-`` as custom elements
+          isCustomElement: (tag) => tag.includes('lr-')
+        }
+      }
+    }),
   ],
   resolve: {
     alias: {
