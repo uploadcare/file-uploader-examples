07ca9c67af25399202b793fc7dd1e8900a13164d
igoradamenko <mail@igoradamenko.com>
2023-11-27 01:15:06 +0200

    Implement App component for the Angular example

--- examples/angular-uploader/src/app/app.component.ts
+++ examples/angular-uploader/src/app/app.component.ts
@@ -1,14 +1,56 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { JsonPipe } from '@angular/common';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { OutputFileEntry } from '@uploadcare/blocks';
+
+import MOCK_DATA from '../mocks';
+
+type FormType = {
+  title: string;
+  text: string;
+  photos: OutputFileEntry[];
+}
 
 @Component({
-  selector: 'app-root',
+  selector: 'app',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    ReactiveFormsModule,
+    JsonPipe,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'angular-uploader';
+  title= new FormControl<string>(MOCK_DATA.title, { nonNullable: true });
+  text= new FormControl<string>(MOCK_DATA.text, { nonNullable: true });
+  photos = MOCK_DATA.photos;
+
+  sentFormObject: FormType | null = null;
+
+  theme: 'light' | 'dark' = 'light'
+
+  handleFormSubmit = (e: SubmitEvent) => {
+    e.preventDefault();
+
+    this.sentFormObject = {
+      title: this.title.value,
+      text: this.text.value,
+      photos: this.photos,
+    };
+  }
+
+  handleThemeChange = (e: Event) => {
+    if (!(e.target instanceof HTMLInputElement)) return;
+
+    this.theme = e.target.checked ? 'light' : 'dark';
+
+    this.updateDocumentTheme();
+  }
+
+  updateDocumentTheme() {
+    document.body.classList.remove('theme--light');
+    document.body.classList.remove('theme--dark');
+    document.body.classList.add(`theme--${this.theme}`);
+  }
 }
