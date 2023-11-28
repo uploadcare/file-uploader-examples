123cd7e2e1dc27564d9839090def5f8855ff98df
igoradamenko <mail@igoradamenko.com>
2023-11-28 20:16:19 +0200

    Add Angular minimal and regular examples

---
+++ examples/angular-uploader/minimal/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import * as LR from '@uploadcare/blocks';
+
+import blocksStyles from '@uploadcare/blocks/web/lr-file-uploader-minimal.min.css?url';
+
+LR.registerBlocks(LR);
+
+@Component({
+  selector: 'app',
+  standalone: true,
+  templateUrl: './app.component.html',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+})
+export class AppComponent {
+  blocksStyles = blocksStyles;
+}
