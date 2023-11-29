ffce2e34f4918817022ff2bd525ab46abe57ae11
igoradamenko <mail@igoradamenko.com>
2023-11-29 13:26:11 +0200

    Replace gh-pages setup with a simple build

--- examples/next-uploader/regular/next.config.js
+++ examples/next-uploader/regular/next.config.js
@@ -5,8 +5,6 @@ const { PHASE_PRODUCTION_BUILD } = require('next/constants')
  */
 const nextConfig = {
   output: 'export',
-  distDir: 'dist',
-  basePath: `${process.env.BASE_PATH || '/'}next-uploader/regular`
 };
 
 module.exports = (phase) => {
