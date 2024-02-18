a1a271fb36f27d96b6c004f041bf73d7f14c5d06
igoradamenko <mail@igoradamenko.com>
2024-02-19 01:23:07 +0200

    Add previews to minimal example of next-uploader

--- examples/next-uploader/pages/minimal.js
+++ examples/next-uploader/pages/minimal.js
@@ -1,9 +1,37 @@
 import * as LR from '@uploadcare/blocks';
 import { PACKAGE_VERSION } from '@uploadcare/blocks';
+import React, { useEffect, useRef, useState } from 'react';
+
+import st from './styles.module.css';
 
 LR.registerBlocks(LR);
 
 function Minimal() {
+  const [files, setFiles] = useState([]);
+  const ctxProviderRef = useRef(null);
+
+  useEffect(() => {
+    const ctxProvider = ctxProviderRef.current;
+    if (!ctxProvider) return;
+
+    const handleChangeEvent = (e) => {
+      console.log('change event payload:', e);
+
+      setFiles([...e.detail.allEntries.filter(f => f.status === 'success')]);
+    };
+
+    /*
+      Note: Event binding is the main way to get data and other info from File Uploader.
+      There plenty of events you may use.
+
+      See more: https://uploadcare.com/docs/file-uploader/events/
+     */
+    ctxProvider.addEventListener('change', handleChangeEvent);
+    return () => {
+      ctxProvider.removeEventListener('change', handleChangeEvent);
+    };
+  }, [setFiles]);
+
   return (
     <div>
       <lr-config
@@ -14,6 +42,30 @@ function Minimal() {
         ctx-name="my-uploader"
         css-src={`https://cdn.jsdelivr.net/npm/@uploadcare/blocks@${PACKAGE_VERSION}/web/lr-file-uploader-minimal.min.css`}
       ></lr-file-uploader-minimal>
+      <lr-upload-ctx-provider
+        ctx-name="my-uploader"
+        ref={ctxProviderRef}
+      ></lr-upload-ctx-provider>
+
+      <div className={st.previews}>
+        {files.map((file) => (
+          <div key={file.uuid}>
+            <img
+              className={st.previewImage}
+              key={file.uuid}
+              src={`${file.cdnUrl}/-/preview/-/resize/x400/`}
+              width="200"
+              height="200"
+              alt={file.fileInfo.originalFilename || ''}
+              title={file.fileInfo.originalFilename || ''}
+            />
+
+            <span className={st.previewTitle}>
+              {file.fileInfo.originalFilename}, {formatSize(file.fileInfo.size)}
+            </span>
+          </div>
+        ))}
+      </div>
 
       <p>
         <a href="/">← All Next.js Examples</a>
@@ -23,3 +75,14 @@ function Minimal() {
 }
 
 export default Minimal;
+
+function formatSize(bytes) {
+  if (!bytes) return '0 Bytes';
+
+  const k = 1024;
+  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+  return `${parseFloat((bytes / k ** i).toFixed(2))} ${sizes[i]}`;
+}
