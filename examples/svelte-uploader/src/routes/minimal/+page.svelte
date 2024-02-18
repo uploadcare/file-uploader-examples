4b7cdbe7feab0f3ec19ce73703c611ecdb401daf
igoradamenko <mail@igoradamenko.com>
2024-02-19 01:55:33 +0200

    Add previews to minimal example of svelte-uploader

--- examples/svelte-uploader/src/routes/minimal/+page.svelte
+++ examples/svelte-uploader/src/routes/minimal/+page.svelte
@@ -1,8 +1,45 @@
 <script>
   import * as LR from '@uploadcare/blocks';
   import blocksStyles from '@uploadcare/blocks/web/lr-file-uploader-minimal.min.css?url';
+  import { onMount } from 'svelte';
 
-  LR.registerBlocks(LR);
+  export let files = [];
+
+  let ctxProviderRef;
+
+  const handleChangeEvent = e => {
+    console.log('change event payload:', e);
+
+    if (e.detail) {
+      files = e.detail.allEntries.filter(f => f.status === 'success');
+    }
+  };
+
+  onMount(() => {
+    LR.registerBlocks(LR);
+
+    /*
+      Note: Event binding is the main way to get data and other info from File Uploader.
+      There plenty of events you may use.
+
+      See more: https://uploadcare.com/docs/file-uploader/events/
+     */
+    ctxProviderRef.addEventListener('change', handleChangeEvent);
+
+    return () => {
+      ctxProviderRef.removeEventListener('change', handleChangeEvent);
+    };
+  });
+  function formatSize(bytes) {
+    if (!bytes) return '0 Bytes';
+
+    const k = 1024;
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+    return `${parseFloat((bytes / k ** i).toFixed(2))} ${sizes[i]}`;
+  }
 </script>
 
 <div>
@@ -15,4 +52,47 @@
     ctx-name="my-uploader"
     css-src={blocksStyles}
   ></lr-file-uploader-minimal>
+
+  <lr-upload-ctx-provider
+    ctx-name="my-uploader"
+    bind:this={ctxProviderRef}
+  ></lr-upload-ctx-provider>
+
+  <div class="previews">
+    {#each files as file (file.cdnUrl)}
+      <div>
+        <img
+          class="preview-image"
+          src={`${file.cdnUrl}/-/preview/-/resize/x400/`}
+          width="200"
+          height="200"
+          alt={file.fileInfo.originalFilename}
+          title={file.fileInfo.originalFilename}
+        />
+
+        <span class="preview-title">
+          {file.fileInfo.originalFilename}, {formatSize(file.fileInfo.size)}
+        </span>
+      </div>
+    {/each}
+  </div>
 </div>
+
+<style>
+  .previews {
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+    width: 100%;
+    margin-top: 32px;
+  }
+
+  .preview-image {
+    display: block;
+    object-fit: cover;
+  }
+
+  .preview-title {
+    margin-top: 4px;
+  }
+</style>
