c219b7141fb98579638e81b9fec11d7c1a50b85b
igoradamenko <mail@igoradamenko.com>
2024-02-19 02:09:45 +0200

    Add previews to minimal example of vue-uploader

--- examples/vue-uploader/src/views/MinimalView/MinimalView.vue
+++ examples/vue-uploader/src/views/MinimalView/MinimalView.vue
@@ -8,8 +8,43 @@ export default {
   data() {
     return {
       blocksStyles,
+      files: [],
     }
   },
+
+  methods: {
+    handleChangeEvent(e) {
+      console.log('change event payload:', e);
+
+      if (e.detail) {
+        this.files = e.detail.allEntries.filter(f => f.status === 'success');
+      }
+    },
+    formatSize(bytes) {
+      if (!bytes) return '0 Bytes';
+
+      const k = 1024;
+      const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+
+      const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+      return `${parseFloat((bytes / k ** i).toFixed(2))} ${sizes[i]}`;
+    }
+  },
+
+  mounted() {
+    /*
+      Note: Event binding is the main way to get data and other info from File Uploader.
+      There plenty of events you may use.
+
+      See more: https://uploadcare.com/docs/file-uploader/events/
+     */
+    this.$refs.ctxProviderRef.addEventListener('change', this.handleChangeEvent);
+  },
+
+  beforeUnmount() {
+    this.$refs.ctxProviderRef.removeEventListener('change', this.handleChangeEvent);
+  }
 }
 </script>
 
@@ -24,5 +59,49 @@ export default {
       ctx-name="my-uploader"
       :css-src="blocksStyles"
     ></lr-file-uploader-minimal>
+
+    <lr-upload-ctx-provider
+      ctx-name="my-uploader"
+      ref="ctxProviderRef"
+    ></lr-upload-ctx-provider>
+
+    <div class="previews">
+      <div
+        v-for="file in files"
+        :key="file.cdnUrl"
+      >
+        <img
+          class="preview-image"
+          :src="`${file.cdnUrl}/-/preview/-/resize/x400/`"
+          width="200"
+          height="200"
+          :alt="file.fileInfo.originalFilename"
+          :title="file.fileInfo.originalFilename"
+        />
+
+        <span class="preview-title">
+          {{file.fileInfo.originalFilename}}, {{formatSize(file.fileInfo.size)}}
+        </span>
+      </div>
+    </div>
   </div>
 </template>
+
+<style scoped>
+.previews {
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  width: 100%;
+  margin-top: 32px;
+}
+
+.preview-image {
+  display: block;
+  object-fit: cover;
+}
+
+.preview-title {
+  margin-top: 4px;
+}
+</style>
