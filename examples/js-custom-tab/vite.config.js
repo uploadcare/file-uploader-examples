4f2c886a69415faafa6f30304227f611217e1d5d
nd0ut <nd0ut@users.noreply.github.com>
2024-06-21 16:07:24 +0300

    Fix custom tab build

--- examples/js-custom-tab/vite.config.js
+++ examples/js-custom-tab/vite.config.js
@@ -1,13 +1,12 @@
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
+import { resolve } from "path";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        'custom-source': resolve(__dirname, 'src/custom-source/index.html'),
+        main: resolve(__dirname, "index.html"),
       },
     },
   },
-})
+});
