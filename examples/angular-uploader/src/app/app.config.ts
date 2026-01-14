071f181040e8240959983bed02d6bf8a3752ee69
igoradamenko <mail@igoradamenko.com>
2026-01-15 00:14:04 +0200

    Update Angular to v20 in angular-uploader

--- examples/angular-uploader/src/app/app.config.ts
+++ examples/angular-uploader/src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -6,6 +6,6 @@ import { routes } from './app.routes';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideExperimentalZonelessChangeDetection(),
+    provideZonelessChangeDetection(),
   ]
 };
