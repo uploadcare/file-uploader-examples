a5855562b4878b10f2bb27e7135af7c222662260
igoradamenko <mail@igoradamenko.com>
2026-01-14 22:56:14 +0200

    Update Angular to v18 in angular-uploader

--- examples/angular-uploader/src/app/app.config.ts
+++ examples/angular-uploader/src/app/app.config.ts
@@ -1,8 +1,11 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideExperimentalZonelessChangeDetection(),
+  ]
 };
